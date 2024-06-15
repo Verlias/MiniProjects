6019f73d7eec2b2f3e16f7b330157eebfede908f
Logan <loganvoravong@gmail.com>
2024-06-15 17:16:00 -0400

    .

--- HelloWorld-C#/HelloWorld-C#/obj/Debug/net8.0/HelloWorld-C#.AssemblyInfo.cs
+++ HelloWorld-C#/HelloWorld-C#/obj/Debug/net8.0/HelloWorld-C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld-C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e9e833a8ad5ed7fe8936ca14122b7f6f690209")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c99b097fb974e478aa2d2c574eaeecca6a1e9f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld-C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld-C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

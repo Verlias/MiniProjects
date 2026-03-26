fe275efa57717ccc1f92f3a6c01b7655d34da229
Verlias <loganvoravong@gmail.com>
2026-03-25 23:56:28 -0400

    ble

---
+++ C# Projects/LearnC#inOneDay/Ch2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            // Console.Read();
+            // byte userAge = 20; 
+            // int numberOfEmployees = 510;
+
+            //Would get an error if this is written
+            // byte userAge2 = 20.0;
+            //Outputs 3
+            Console.WriteLine(7 / 2);
+            //Outputs 3.5
+            Console.WriteLine(7 / 2.0);
+            //Outputs 3.5
+            Console.WriteLine(7.0 / 2.0);
+            int x = 20;
+            Console.WriteLine(x);
+            int y = (int) 20.9;
+            Console.WriteLine(y);
+
+            Console.WriteLine("----");
+            int[] userAge = {21, 22, 23, 24, 25};
+            foreach (int age in userAge)
+            {
+                Console.WriteLine(age);
+            }
+
+            //Holds a fixed number of values.
+            int [] myArray = new int[10];
+            Console.WriteLine(myArray);
+
+            //Dynamic array
+            List<int> userAgeList = new List<int> {11, 21, 31, 41};
+            Console.WriteLine(userAgeList);
+
+        }
+    }
+}
